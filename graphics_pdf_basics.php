2173677549a98021b6c8193e6503901140cbe711
neMagrel <markokcat@yandex.ru>
2025-12-25 23:55:54 +0500

    ref 2

--- graphics_pdf_basics.php
+++ graphics_pdf_basics.php
@@ -1,10 +1,10 @@
 <?php
+declare(strict_types=1);
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
-declare(strict_types=1);
 
 header('Content-Type: text/html; charset=utf-8');
 
