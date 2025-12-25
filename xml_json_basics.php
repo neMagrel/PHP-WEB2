1a9e64591087fc73aba4c6474f826a8b31523c3c
neMagrel <markokcat@yandex.ru>
2025-12-26 03:33:03 +0500

    ref 1 - 12

--- xml_json_basics.php
+++ xml_json_basics.php
@@ -1,6 +1,9 @@
 <?php
-
 declare(strict_types=1);
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
 
 /**
  * xml_json_basics.php — лабораторная по работе с XML и JSON.
