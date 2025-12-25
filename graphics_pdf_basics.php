6c3bef497d5df0833909aaeb7a38c1812a1c9f30
neMagrel <markokcat@yandex.ru>
2025-12-25 23:18:45 +0500

    refactoring 11

--- graphics_pdf_basics.php
+++ graphics_pdf_basics.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
 declare(strict_types=1);
 
 header('Content-Type: text/html; charset=utf-8');
